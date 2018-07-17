21d99c1633cd0706b8c9e5ca47df44f73ccf9132
Carlos <cbcarlos08@gmail.com>
2018-07-17 15:50:22 -0400

    First commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-var gulp = require('gulp');
+var gulp   = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 
 gulp.task('concat', function() {
-    return gulp.src('./build/*.js')
-        .pipe(concat('made-with-love.js'))
-        .pipe(gulp.dest('./dist/'));
+    return gulp.src('./dist/radio-custom-element/*.js')
+        .pipe(concat('rating.js'))
+        .pipe(gulp.dest('./dist/element'));
 });
 
 gulp.task('rename', function() {
-    return gulp.src('./dist/**/*.css')
+    return gulp.src('./dist/radio-custom-element/*.css')
         .pipe(rename('rating.css'))
         .pipe(gulp.dest('./dist/element'));
 });
 //fonts
 gulp.task('fonts', function () {
-  return gulp.src(['./dist/radio-custom-element/font=awesome-webfont.*'])
+  return gulp.src(['./dist/radio-custom-element/font-awesome-webfont.*'])
       .pipe(gulp.dest('./dist/element'))
 })
 
